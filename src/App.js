74bf90f8f35ff739bb5d817bbc819230a1ac04e2
brettshackett <63305041+brettshackett@users.noreply.github.com>
2023-08-19 14:19:50 -0400

    First commit, friend tracker app from CBT Nuggets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Friend Tracker</h1>
+      <div>
+        <img 
+          src= {process.env.PUBLIC_URL + '/Stock_Headshot.jpg'}
+          alt="Brett Headshot"
+          height = "200" />
+          <h2>My Profile</h2>
+          <h3>Name</h3>
+          <p>Brett Shackett</p>
+          <h3>Age</h3>
+          <p>25</p>
+          <h3>Bio</h3>
+          <p>I like to program, I also like food.</p>
+          <h3>Birthday</h3>
+          <p>December 9, 1997</p>
+          <h3>Interests</h3>
+          <ul>
+            <li>Programming</li>
+            <li>Gaming</li>
+            <li>Golfing</li>
+            <li>Sleeping</li>
+          </ul>
+      </div>
     </div>
   );
 }
